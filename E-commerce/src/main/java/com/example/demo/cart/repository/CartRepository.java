823cfbc5c252b5e96c8854842bd70bb6d746496a
Akashr241 <akashr.offical7@gmail.com>
2026-05-14 08:31:19 +0530

    interface cart service method used matching CartServiceimpl

--- E-commerce/src/main/java/com/example/demo/cart/repository/CartRepository.java
+++ E-commerce/src/main/java/com/example/demo/cart/repository/CartRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.cart.repository;
 import com.example.demo.cart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
    // Optional<Cart> findByUserId(Long userId);
