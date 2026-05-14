33179b514d8bfd65bc3b8b0aefcb341e0901856b
Akashr241 <akashr.offical7@gmail.com>
2026-05-14 10:30:47 +0530

    to product cart in postman diasbale the cart

--- E-commerce/src/main/java/com/example/demo/config/SecurityConfig.java
+++ E-commerce/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,24 +1,24 @@
-
 package com.example.demo.config;
-import org.springframework.context.annotation.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
         http
-            .csrf(csrf -> csrf.disable())  
-         //   .sessionManagement(session -> session
-           //     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            //)
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/cart/**").permitAll() // allow your API
-                .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
