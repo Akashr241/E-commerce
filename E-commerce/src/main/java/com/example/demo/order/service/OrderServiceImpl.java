37bd79fb78af05b4c980034384fc90b311d371f2
Akashr241 <akashr.offical7@gmail.com>
2026-05-15 14:15:48 +0530

    create order file structure

---
+++ E-commerce/src/main/java/com/example/demo/order/service/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.order.service;
+import com.example.demo.order.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+  
+}
