621980b4b0cfe30b79ccf4012eb36aa25a68350e
Akashr241 <akashr.offical7@gmail.com>
2026-05-18 11:00:23 +0530

    "implement initial order service logic"

--- E-commerce/src/main/java/com/example/demo/order/service/OrderServiceImpl.java
+++ E-commerce/src/main/java/com/example/demo/order/service/OrderServiceImpl.java
@@ -1,27 +1,39 @@
 package com.example.demo.order.service;
 import com.example.demo.order.dto.OrderResponseDto;
+import com.example.demo.order.entity.Order;
+import com.example.demo.order.mapper.OrderMapper;
 import com.example.demo.order.repository.OrderRepository;
 import com.example.demo.order.service.OrderService;
+
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
+import com.example.demo.cart.entity.Cart;
+import com.example.demo.cart.repository.CartRepository;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final CartRepository cartRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            CartRepository cartRepository
+    ) {
         this.orderRepository = orderRepository;
+        this.cartRepository = cartRepository;
     }
 
     @Override
     public OrderResponseDto placeOrder(Long cartId) {
-
-        OrderResponseDto dto = new OrderResponseDto();
-
-        dto.setId(1L);
+        Cart cart = cartRepository.findById(cartId)
+                .orElseThrow(() -> new RuntimeException("Cart not found with id: "));
+        Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
+        OrderResponseDto dto =new OrderResponseDto();
         dto.setStatus("PLACED");
-        dto.setTotalAmount(255000.0);
+        Order savedOrder = orderRepository.save(order);
 
-        return dto;
+        return OrderMapper.mapToOrderResponseDto(savedOrder);
     }
 }
