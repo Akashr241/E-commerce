e9ec78bc3bcdffa73930891d22ff300787a25fde
Akashr241 <akashr.offical7@gmail.com>
2026-05-23 09:45:04 +0530

    connect to order to cartfile,

--- E-commerce/src/main/java/com/example/demo/order/service/OrderServiceImpl.java
+++ E-commerce/src/main/java/com/example/demo/order/service/OrderServiceImpl.java
@@ -4,12 +4,15 @@ import com.example.demo.order.entity.Order;
 import com.example.demo.order.mapper.OrderMapper;
 import com.example.demo.order.repository.OrderRepository;
 import com.example.demo.order.service.OrderService;
-
+import java.util.ArrayList;
 import java.time.LocalDateTime;
-
+import java.util.List;
 import org.springframework.stereotype.Service;
 import com.example.demo.cart.entity.Cart;
 import com.example.demo.cart.repository.CartRepository;
+import com.example.demo.order.entity.OrderItem;
+//import com.example.demo.cart.entity.Cart;
+import com.example.demo.cartitem.CartItem;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -31,10 +34,26 @@ public class OrderServiceImpl implements OrderService {
         Order order = new Order();
         order.setOrderDate(LocalDateTime.now());
         OrderResponseDto dto =new OrderResponseDto();
-        dto.setStatus("PLACED");
+        order.setStatus("PLACED");
+                order.setTotalAmount(cart.getTotalPrice());
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (CartItem cartItem : cart.getCartItems()) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(cartItem.getProduct());
+            orderItem.setProductName(cartItem.getProduct().getName());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setPrice(cartItem.getProduct().getPrice());
+            orderItem.setSubtotal(cartItem.getSubTotal());
+            orderItem.setOrder(order);
+            orderItems.add(orderItem);
+            order.setOrderItems(orderItems);
+            
+        }
+        order.setOrderItems(orderItems);
         Order savedOrder = orderRepository.save(order);
 
         return OrderMapper.mapToOrderResponseDto(savedOrder);
     }
     
+
 }
