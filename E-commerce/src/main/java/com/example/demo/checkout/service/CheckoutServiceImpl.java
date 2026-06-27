1f2499bee0d909fb64d981b140c29511b607d276
Akashr241 <akashr.offical7@gmail.com>
2026-06-27 14:01:39 +0530

    checkoutpage link the cart

--- E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
+++ E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.checkout.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import com.example.demo.security.user.repository.UserRepository;
 import com.example.demo.cart.dto.CartItemResponseDto;
@@ -10,36 +11,48 @@ import com.example.demo.cart.entity.Cart;
 import com.example.demo.cart.repository.CartRepository;
 import com.example.demo.checkout.dto.CheckoutRequestDto;
 import com.example.demo.checkout.dto.CheckoutResponseDto;
-
+import com.example.demo.security.user.entity.User;
+import com.example.demo.security.user.repository.UserRepository;
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
     @Autowired
     private CartRepository cartRepository;
+    @Autowired
+    private UserRepository userRepository;
     
  
     @Override
     public CheckoutResponseDto checkout(CheckoutRequestDto request) {
 
-        // Replace this with the logged-in user's cart
-        Long userId = 1L;
 
-        Cart cart = cartRepository.findByUserId(userId)
+
+       String email = SecurityContextHolder
+        .getContext()
+        .getAuthentication()
+        .getName();
+
+User user = userRepository.findByEmail(email)
+        .orElseThrow(() ->
+                new RuntimeException("User not found"));
+
+
+        Cart cart = cartRepository.findByUserId(request.getCartId())
                 .orElseThrow(() -> new RuntimeException("Cart not found"));
 
-        List<CartItemResponseDto> items = cart.getItems()
+        List<CartItemResponseDto> items = cart.getCartItems()
                 .stream()
-                .map(item -> {
-                    CartItemResponseDto dto = new CartItemResponseDto();
-                    dto.setProductId(item.getProduct().getId());
-                    dto.setProductName(item.getProduct().getName());
-                    dto.setPrice(item.getProduct().getPrice());
-                    dto.setQuantity(item.getQuantity());
-                    return dto;
-                })
+                .map(item -> 
+                     new CartItemResponseDto(;
+                    item.getId();
+                    item.getProduct().getName();
+                    item.getSubTotal();
+                    item.getQuantity();
+                
+                ))
                 .toList();
 
-        double total = cart.getItems()
+        double total = cart.getCartItems()
                 .stream()
                 .mapToDouble(item ->
                         item.getProduct().getPrice() * item.getQuantity())
