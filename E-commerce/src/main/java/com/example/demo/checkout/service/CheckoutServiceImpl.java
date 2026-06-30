c16ac9553226ef302cd9a118d79cd5b850726a26
Akashr241 <akashr.offical7@gmail.com>
2026-06-30 12:28:10 +0530

    cart respository change save the user

--- E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
+++ E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
@@ -12,22 +12,22 @@ import com.example.demo.cart.repository.CartRepository;
 import com.example.demo.checkout.dto.CheckoutRequestDto;
 import com.example.demo.checkout.dto.CheckoutResponseDto;
 import com.example.demo.security.user.entity.User;
-import com.example.demo.security.user.repository.UserRepository;
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
-    @Autowired
-    private CartRepository cartRepository;
-    @Autowired
-    private UserRepository userRepository;
-    
- 
+   public CheckoutServiceImpl(
+            CartRepository cartRepository,
+            UserRepository userRepository) {
+
+        this.cartRepository = cartRepository;
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public CheckoutResponseDto checkout(CheckoutRequestDto request) {
 
-
-
-       String email = SecurityContextHolder
+String email = SecurityContextHolder
         .getContext()
         .getAuthentication()
         .getName();
@@ -36,21 +36,21 @@ User user = userRepository.findByEmail(email)
         .orElseThrow(() ->
                 new RuntimeException("User not found"));
 
+Cart cart = cartRepository.findByUser(user)
+        .orElseThrow(() ->
+                new RuntimeException("Cart not found"));
 
-        Cart cart = cartRepository.findByUserId(request.getUserId())
-                .orElseThrow(() -> new RuntimeException("Cart not found"));
+List<CartItemResponseDto> items = cart.getCartItems()
+        .stream()
+        .map(item -> new CartItemResponseDto(
+                item.getId(),
+                item.getProduct().getName(),
+                item.getQuantity(),
+                item.getSubTotal()
+        ))
+        .toList();
 
-        List<CartItemResponseDto> items = cart.getCartItems()
-                .stream()
-                .map(item -> 
-                     new CartItemResponseDto(;
-                    item.getId();
-                    item.getProduct().getName();
-                    item.getSubTotal();
-                    item.getQuantity();
-                
-                ))
-                .toList();
+    
 
         double total = cart.getCartItems()
                 .stream()
