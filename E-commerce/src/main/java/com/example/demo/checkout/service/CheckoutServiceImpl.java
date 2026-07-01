358553754eb4e9122d43753ef600426e898b25b7
Akashr241 <akashr.offical7@gmail.com>
2026-07-01 13:22:06 +0530

    test api

--- E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
+++ E-commerce/src/main/java/com/example/demo/checkout/service/CheckoutServiceImpl.java
@@ -2,20 +2,24 @@ package com.example.demo.checkout.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import com.example.demo.security.user.repository.UserRepository;
+
 import com.example.demo.cart.dto.CartItemResponseDto;
 import com.example.demo.cart.entity.Cart;
 import com.example.demo.cart.repository.CartRepository;
 import com.example.demo.checkout.dto.CheckoutRequestDto;
 import com.example.demo.checkout.dto.CheckoutResponseDto;
 import com.example.demo.security.user.entity.User;
+import com.example.demo.security.user.repository.UserRepository;
+
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
-   public CheckoutServiceImpl(
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+
+    public CheckoutServiceImpl(
             CartRepository cartRepository,
             UserRepository userRepository) {
 
@@ -23,45 +27,39 @@ public class CheckoutServiceImpl implements CheckoutService {
         this.userRepository = userRepository;
     }
 
-
     @Override
-    public CheckoutResponseDto checkout(CheckoutRequestDto request) {
+    public CheckoutResponseDto checkout(
+            CheckoutRequestDto request) {
 
-String email = SecurityContextHolder
-        .getContext()
-        .getAuthentication()
-        .getName();
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
 
-User user = userRepository.findByEmail(email)
-        .orElseThrow(() ->
-                new RuntimeException("User not found"));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
 
-Cart cart = cartRepository.findByUser(user)
-        .orElseThrow(() ->
-                new RuntimeException("Cart not found"));
+        Cart cart = cartRepository.findByUser(user)
+                .orElseThrow(() ->
+                        new RuntimeException("Cart not found"));
 
-List<CartItemResponseDto> items = cart.getCartItems()
-        .stream()
-        .map(item -> new CartItemResponseDto(
-                item.getId(),
-                item.getProduct().getName(),
-                item.getQuantity(),
-                item.getSubTotal()
-        ))
-        .toList();
+        List<CartItemResponseDto> items =
+                cart.getCartItems()
+                        .stream()
+                        .map(item ->
+                                new CartItemResponseDto(
+                                        item.getId(),
+                                        item.getProduct().getName(),
+                                        item.getQuantity(),
+                                        item.getSubTotal()))
+                        .toList();
 
-    
-
-        double total = cart.getCartItems()
-                .stream()
-                .mapToDouble(item ->
-                        item.getProduct().getPrice() * item.getQuantity())
-                .sum();
-
-        CheckoutResponseDto response = new CheckoutResponseDto();
+        CheckoutResponseDto response =
+                new CheckoutResponseDto();
 
         response.setItems(items);
-        response.setTotal(total);
+        response.setTotal(cart.getTotalPrice());
         response.setShippingAddress(request);
 
         return response;
