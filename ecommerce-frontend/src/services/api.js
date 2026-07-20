93ae86ad8fac799250f2585fe2db2b27f5c5bede
Akashr241 <akashr.offical7@gmail.com>
2026-07-20 12:29:09 +0530

    we use backend url

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+  baseURL: "https://e-commerce-backend-akash.onrender.com"
 });
 
 API.interceptors.request.use(
