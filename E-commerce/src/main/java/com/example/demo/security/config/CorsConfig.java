e4640a00e2ad7d0c39a0ebbd5a5aa6ca13e64048
Akashr241 <akashr.offical7@gmail.com>
2026-08-21 06:44:08 +0530

    fix the connect to the backend to frontend

--- E-commerce/src/main/java/com/example/demo/security/config/CorsConfig.java
+++ E-commerce/src/main/java/com/example/demo/security/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://e-commerce-zeta-sepia-33.vercel.app"));
+        config.setAllowedOrigins(List.of("https://e-commerce-zeta-sepia-33.vercel.app","http://localhost:3000"));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+        baseURL: "http://localhost:8080",
+
+
 });
 
 API.interceptors.request.use(
