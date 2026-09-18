9f1f696b0dd8c3a33edcdd6ef6d77ecb16e48e44
Akashr241 <akashr.offical7@gmail.com>
2026-09-18 19:39:47 +0530

    Add logging for API requests in authService and remove unused import in ProductRepository

--- ecommerce-frontend/src/services/authService.js
+++ ecommerce-frontend/src/services/authService.js
@@ -9,6 +9,12 @@ export const registerUser = async (userData) => {
             "/auth/register",
             userData
         );
+        
+console.log("========== API REQUEST ==========");
+console.log("Base URL:", config.baseURL);
+console.log("URL:", config.url);
+console.log("FULL URL:", config.baseURL + config.url);
+console.log("JWT Token:", token);
 
         return response.data;
 
@@ -45,6 +51,11 @@ export const loginUser = async (credentials) => {
             "/auth/login",
             credentials
         );
+        console.log("========== API REQUEST ==========");
+console.log("Base URL:", config.baseURL);
+console.log("URL:", config.url);
+console.log("FULL URL:", config.baseURL + config.url);
+console.log("JWT Token:", token);
 
         return response.data;
 
