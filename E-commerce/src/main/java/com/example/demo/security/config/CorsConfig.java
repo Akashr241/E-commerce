02d8c901c16184c805c55395956c01ba8cb90238
Akashr241 <akashr.offical7@gmail.com>
2026-09-18 22:33:55 +0530

    Refactor CORS configuration to use CorsConfigurationSource and clean up unused code

--- E-commerce/src/main/java/com/example/demo/security/config/CorsConfig.java
+++ E-commerce/src/main/java/com/example/demo/security/config/CorsConfig.java
@@ -1,34 +1,58 @@
 package com.example.demo.security.config;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import com.google.api.client.util.Value;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
-        @Value("${app.frontend.url:http://localhost:3000}")
-        private String frontendUrl;
+
+    @Value("${app.frontend.url:http://localhost:3000}")
+    private String frontendUrl;
+
 
     @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        
-     
-        config.setAllowedOrigins(List.of("frotendUrl"));
+    public CorsConfigurationSource corsConfigurationSource() {
 
+        CorsConfiguration configuration =
+                new CorsConfiguration();
 
+        configuration.setAllowedOrigins(
+                List.of(
+                        frontendUrl
+                )
+        );
 
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+        );
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
 
-        return new CorsFilter(source);
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
     }
 }
